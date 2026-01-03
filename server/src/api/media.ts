435b3204016159091e798bb093822d79f55aff91
EAGOWL-POC Team <team@eagowl-poc.com>
2026-01-03 13:43:36 -0500

    first commit

--- server/src/api/media.ts
+++
@@ -1,9 +0,0 @@
-import { FastifyRequest, FastifyReply } from 'fastify';
-
-export async function uploadMedia(request: FastifyRequest, reply: FastifyReply) {
-  // Mock upload endpoint
-  return reply.send({
-    success: true,
-    message: 'Media uploaded successfully'
-  });
-}
